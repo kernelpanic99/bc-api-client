7d6ea758ef6cfd269c25650b5e52aebfedca44aa
Arthur Nikolaienko <kernelpanic99.npm@pm.me>
2025-05-07 23:20:11 +0300

    Refactor BigCommerceClient to enhance concurrency handling and add collectV2 method for collecting v2 endpoints. Updated concurrent error handling

--- src/client.ts
+++ src/client.ts
@@ -1,5 +1,5 @@
 import { V3Resource } from './core';
-import { RateLimitOptions, RequestOptions, StoreOptions, request } from './net';
+import { RateLimitOptions, RequestError, RequestOptions, StoreOptions, request } from './net';
 import { chunk, range } from 'remeda';
 
 const MAX_PAGE_SIZE = 250;
@@ -7,14 +7,19 @@ const MAX_PAGE_SIZE = 250;
 type GetOptions = {
     endpoint: string;
     query?: Record<string, string>;
-    version?: 'v3' | 'v2';
+    version?: 'v2' | 'v3';
 };
 
 type PostOptions<T> = {
     endpoint: string;
     query?: Record<string, string>;
     body: T;
-    version?: 'v3' | 'v2';
+    version?: 'v2' | 'v3';
+};
+
+type ConcurrencyOptions = {
+    concurrency?: number;
+    skipErrors?: boolean;
 };
 
 type Config = StoreOptions & RateLimitOptions;
@@ -54,28 +59,43 @@ export class BigCommerceClient {
         });
     }
 
-    async concurrent<T, R>(requests: RequestOptions<T>[], concurrency = 10): Promise<R[]> {
-        const chunks = chunk(requests, concurrency);
+    async concurrent<T, R>(requests: RequestOptions<T>[], options: ConcurrencyOptions): Promise<R[]> {
+        const chunks = chunk(requests, options.concurrency ?? 10);
+        const skipErrors = options.skipErrors ?? false;
 
-        const results = await Promise.all(
-            chunks.map((chunk) =>
-                Promise.all(
-                    chunk.map((opt) =>
-                        request<T, R>({
-                            ...opt,
-                            ...this.config,
-                        }),
-                    ),
+        const results: R[] = [];
+
+        for (const chunk of chunks) {
+            const responses = await Promise.allSettled(
+                chunk.map((opt) =>
+                    request<T, R>({
+                        ...opt,
+                        ...this.config,
+                    }),
                 ),
-            ),
-        );
+            );
 
-        return results.flat();
+            responses.forEach((response) => {
+                if (response.status === 'fulfilled') {
+                    results.push(response.value);
+                } else {
+                    if (!skipErrors) {
+                        throw response.reason;
+                    } else {
+                        console.warn(`Error in concurrent request: ${response.reason}`);
+                    }
+                }
+            });
+        }
+
+        return results;
     }
 
-    async collect<T>(options: Omit<GetOptions, 'version'>, concurrency = 10): Promise<T[]> {
+    async collect<T>(options: Omit<GetOptions, 'version'> & ConcurrencyOptions): Promise<T[]> {
         if (options.query) {
-            options.query.limit = MAX_PAGE_SIZE.toString();
+            if (!options.query.limit) {
+                options.query.limit = MAX_PAGE_SIZE.toString();
+            }
         } else {
             options.query = { limit: MAX_PAGE_SIZE.toString() };
         }
@@ -96,11 +116,61 @@ export class BigCommerceClient {
             query: { ...options.query, page: page.toString() },
         }));
 
-        const responses = await this.concurrent<never, V3Resource<T[]>>(requests, concurrency);
+        const responses = await this.concurrent<never, V3Resource<T[]>>(requests, options);
 
         responses.forEach((response) => {
             results.push(...response.data);
         });
+
+        return results;
+    }
+
+    async collectV2<T>(options: Omit<GetOptions, 'version'> & ConcurrencyOptions): Promise<T[]> {
+        if (options.query) {
+            if (!options.query.limit) {
+                options.query.limit = MAX_PAGE_SIZE.toString();
+            }
+        } else {
+            options.query = { limit: MAX_PAGE_SIZE.toString() };
+        }
+
+        let done = false;
+        const results: T[] = [];
+        let page = 1;
+        const concurrency = options.concurrency ?? 10;
+
+        while (!done) {
+            const pages = range(page, page + concurrency);
+            page += concurrency;
+
+            const requests = pages.map((page) => ({
+                ...options,
+                version: 'v2' as const,
+                query: { ...options.query, page: page.toString() },
+            }));
+
+            const responses = await Promise.allSettled(requests.map((request) => this.get<T[]>(request)));
+
+            responses.forEach((response) => {
+                if (response.status === 'fulfilled') {
+                    if (response.value) {
+                        results.push(...response.value);
+                    } else {
+                        done = true;
+                    }
+                } else {
+                    if (response.reason instanceof RequestError && response.reason.status === 404) {
+                        done = true;
+                    } else {
+                        if (!options.skipErrors) {
+                            throw response.reason;
+                        } else {
+                            console.warn(`Error in collectV2: ${response.reason}`);
+                        }
+                    }
+                }
+            });
+        }
 
         return results;
     }
