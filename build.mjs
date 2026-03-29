19d5502f9a077fcb74222ef64087e77a5520d736
Arthur Nikolaienko <kernelpanic99.npm@pm.me>
2026-03-29 10:21:29 +0300

    Move legacy code and tests under v2 submodule

--- build.mjs
+++ build.mjs
@@ -1,14 +1,27 @@
-import { build } from 'esbuild';
-import dtsPlugin from 'esbuild-plugin-d.ts';
+import { build } from "esbuild";
+import dtsPlugin from "esbuild-plugin-d.ts";
+
+// V2
+await build({
+    entryPoints: ["src/v2/index.ts", "src/v2/endpoints.ts"],
+    outdir: "dist/v2",
+    bundle: true,
+    platform: "node",
+    format: "esm",
+    target: "node20",
+    sourcemap: true,
+    external: ["jose", "ky"],
+    plugins: [dtsPlugin()],
+});
 
 await build({
-    entryPoints: ['src/index.ts', 'src/endpoints.ts'],
-    outdir: 'dist',
+    entryPoints: ["src/index.ts"],
+    outdir: "dist/",
     bundle: true,
-    platform: 'node',
-    format: 'esm',
-    target: 'node20',
+    platform: "node",
+    format: "esm",
+    target: "node20",
     sourcemap: true,
-    external: ['jose', 'ky'],
+    external: ["jose", "ky"],
     plugins: [dtsPlugin()],
 });
--- src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './client';
-export * from './core';
-export * from './auth';
-
-export { Methods } from './net';
