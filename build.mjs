ae431dd65d3ab86704f06221cb480ccaa8e0bc67
Arthur Nikolaienko <kernelpanic99.npm@pm.me>
2026-03-31 19:44:31 +0300

    Lock all dependencies, build tweaks

--- build.mjs
+++ build.mjs
@@ -1,27 +1,14 @@
 import { build } from "esbuild";
 import dtsPlugin from "esbuild-plugin-d.ts";
 
-// V2
-await build({
-    entryPoints: ["src/v2/index.ts", "src/v2/endpoints.ts"],
-    outdir: "dist/v2",
-    bundle: true,
-    platform: "node",
-    format: "esm",
-    target: "node20",
-    sourcemap: true,
-    external: ["jose", "ky"],
-    plugins: [dtsPlugin()],
-});
-
 await build({
     entryPoints: ["src/index.ts"],
     outdir: "dist/",
     bundle: true,
     platform: "node",
     format: "esm",
-    target: "node20",
+    target: "node22",
     sourcemap: true,
-    external: ["jose", "ky"],
+    external: ["jose", "ky", "p-limit"],
     plugins: [dtsPlugin()],
 });
