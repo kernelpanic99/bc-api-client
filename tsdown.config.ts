f17974b2676790e8f851addca538b866fd17bc6b
Arthur Nikolaienko <kernelpanic99.npm@pm.me>
2026-03-31 20:55:38 +0300

    Ditch esbuild in favour of tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig({
+    entry: ['./src/index.ts'],
+    attw: {
+        enabled: true,
+        ignoreRules: ['cjs-resolves-to-esm'],
+    },
+    target: 'node16',
+    format: ['esm'],
+});
