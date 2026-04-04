04dd0ef57b76eafb09498c0e936638916bbd1258
Arthur Nikolaienko <kernelpanic99.npm@pm.me>
2026-04-04 18:22:50 +0300

    Drop legacy unit tests and remove from vite config

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
-        exclude: ['./test/legacy/**'],
         include: ['./test/**.test.ts'],
     },
     resolve: {
